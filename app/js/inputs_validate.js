fc641197d35d73e64ed976c00409103e3dd4e37b
Eugene <1lavrov1@ukr.net>
2017-11-14 16:39:49 +0200

    header with func ++

--- app/js/inputs_validate.js
+++ app/js/inputs_validate.js
@@ -104,6 +104,58 @@ $(document).ready(function(){
         $(this).parent().find('input').val(valueRadio);
     });
 
+    //language select
+
+    // Select
+    $('.language__selected').click(function(){
+        /* Заносим выпадающий список в переменную */
+        var dropBlock = $(this).parent().find('.language__items');
+
+        /* Делаем проверку: Если выпадающий блок скрыт то делаем его видимым*/
+        if( dropBlock.is(':hidden') ) {
+            dropBlock.slideDown();
+
+            /* Выделяем ссылку открывающую select */
+            $(this).addClass('active');
+
+            /* Работаем с событием клика по элементам выпадающего списка */
+            $('.language__items').find('li').click(function(){
+
+                /* Заносим в переменную HTML код элемента
+                списка по которому кликнули */
+                var selectResult = $(this).html();
+
+                /* Находим наш скрытый инпут и передаем в него
+                значение из переменной selectResult */
+                $(this).parent().parent().find('input').val(selectResult);
+
+                /* Передаем значение переменной selectResult в ссылку которая
+                открывает наш выпадающий список и удаляем активность */
+                $('.language__selected').removeClass('active').html(selectResult);
+
+                /* Скрываем выпадающий блок */
+                dropBlock.slideUp();
+            });
+
+            /* Продолжаем проверку: Если выпадающий блок не скрыт то скрываем его */
+        } else {
+            $(this).removeClass('active');
+            dropBlock.slideUp();
+        }
+
+        /* Предотвращаем обычное поведение ссылки при клике */
+        return false;
+    });
+
+
+
+
+
+
+
+
+
+
     $('#myForm').validate({
         rules: {
             email:{
